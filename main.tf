9b93995f46f479e2997a9a430c9f0feee56106df
Roney Santos <joseroneysilvasantos@gmail.com>
2022-11-14 18:09:51 -0300

    create dynamodb and test lock file

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
     bucket = "descomplicando-terraform-roney-tfstates"
+    # dynamodb_table = "terraform-state-lock-dynamo" #lock table DynamoDB
     key    = "terraform-test.tfstate"
     region = "us-east-1"
   }
